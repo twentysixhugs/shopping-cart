19d851aa788ecb15dc94df635dfe3bcea8c7638a
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-13 18:12:40 +0300

    Change CategoryProps interface

--- src/components/Products/Category.tsx
+++ src/components/Products/Category.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 interface CategoryProps {
-  locationName?: number;
+  locationName?: string;
   children?: React.ReactNode;
   name: string;
 }
