f3ff339d23bad0e5ec53ce7bcdf453ac87a6c5c6
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-13 10:32:36 +0300

    Build routing across app

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,23 @@ import './reset.css';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Products from './components/Products/Products';
+import Contact from './components/Contact/Contact';
+import Cart from './components/Cart/Cart';
+import NoMatch from './components/NoMatch/NoMatch';
 
 const root = ReactDOMClient.createRoot(document.querySelector('#root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NoMatch />} />
+      </Route>
     </Routes>
   </BrowserRouter>,
 );
