aac3e4976db65feb39367abb7baa8c9b52fb469b
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-13 10:14:34 +0300

    Build Home

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <div className="c-home">
+      <h1 className="c-home__title">A really clever title</h1>
+      <p className="c-home__description">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Consequatur quia ea at delectus earum iure, nemo dignissimos amet!
+        Inventore modi architecto non repellendus dolore reprehenderit
+        doloremque dicta optio perspiciatis enim! Lorem ipsum dolor sit,
+        amet consectetur adipisicing elit.
+      </p>
+      <Link className="c-home__link" to="/products">
+        Shop now
+      </Link>
+    </div>
+  );
+}
