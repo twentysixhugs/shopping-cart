2f0edbc24393e7b273d2256bb6e8e182cc26255d
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-14 17:02:16 +0300

    Style NoMatch

--- src/components/NoMatch/NoMatch.tsx
+++ src/components/NoMatch/NoMatch.tsx
@@ -1,12 +1,17 @@
 import { useNavigate } from 'react-router-dom';
+import './NoMatch.css';
 
 export default function NoMatch() {
   const navigate = useNavigate();
 
   return (
-    <div style={{ padding: '1rem' }}>
-      <p>There&apos;s nothing here!</p>
-      <button onClick={() => navigate(-1)}>Go back</button>
+    <div className="c-not-found">
+      <div className="c-not-found__wrapper">
+        <p className="c-not-found__text">There&apos;s nothing here!</p>
+        <button className="c-not-found__btn" onClick={() => navigate('/')}>
+          Go to home
+        </button>
+      </div>
     </div>
   );
 }
