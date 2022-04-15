27ae0959970ccf3e88606a35fb672d9ccab4149c
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-15 17:59:15 +0300

    Add decrease and increase handlers to Quantity

--- src/components/Products/Quantity.tsx
+++ src/components/Products/Quantity.tsx
@@ -1,13 +1,25 @@
+import React from 'react';
+
 interface QuantityProps {
   quantity: number;
+  onIncrease: React.MouseEventHandler;
+  onDecrease: React.MouseEventHandler;
 }
 
-export default function Quantity({ quantity }: QuantityProps) {
+export default function Quantity({
+  quantity,
+  onIncrease,
+  onDecrease,
+}: QuantityProps) {
   return (
     <div className="c-quantity-input">
-      <button className="c-quantity-input__btn">-</button>
+      <button className="c-quantity-input__btn" onClick={onDecrease}>
+        -
+      </button>
       <span className="c-quantity-input__quantity">{quantity}</span>
-      <button className="c-quantity-input__btn">+</button>
+      <button className="c-quantity-input__btn" onClick={onIncrease}>
+        +
+      </button>
     </div>
   );
 }
