73fbaac021a449288ab1f3fa09b1403f726b2175
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-15 09:57:13 +0300

    Move routing to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import { Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Products from './components/Products/Products';
+import About from './components/About/About';
+import Cart from './components/Cart/Cart';
+import NoMatch from './components/NoMatch/NoMatch';
+import CurrentCategoryDisplay from './components/Products/CurrentCategoryDisplay';
+import CurrentProduct from './components/Products/CurrentProduct';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Outlet />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />}>
+            <Route
+              path=":categoryName"
+              element={<CurrentCategoryDisplay />}
+            />
+            <Route
+              path=":categoryName/:productId"
+              element={<CurrentProduct />}
+            />
+          </Route>
+          <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/not-found" element={<NoMatch />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,36 +3,6 @@ import ReactDOMClient from 'react-dom/client';
 import './reset.css';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Products from './components/Products/Products';
-import About from './components/About/About';
-import Cart from './components/Cart/Cart';
-import NoMatch from './components/NoMatch/NoMatch';
-import CurrentCategoryDisplay from './components/Products/CurrentCategoryDisplay';
-import CurrentProduct from './components/Products/CurrentProduct';
 
 const root = ReactDOMClient.createRoot(document.querySelector('#root'));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
-        <Route path="/products" element={<Products />}>
-          <Route
-            path=":categoryName"
-            element={<CurrentCategoryDisplay />}
-          />
-          <Route
-            path=":categoryName/:productId"
-            element={<CurrentProduct />}
-          />
-        </Route>
-        <Route path="/about" element={<About />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/not-found" element={<NoMatch />} />
-        <Route path="*" element={<NoMatch />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-);
+root.render(<App />);
