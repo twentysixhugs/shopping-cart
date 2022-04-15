f97ca328a48a6d964b072e0255563e78edb09419
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-15 13:16:30 +0300

    Add saving to cart

--- src/components/Products/CurrentProduct.tsx
+++ src/components/Products/CurrentProduct.tsx
@@ -1,19 +1,41 @@
 import { useParams, Navigate } from 'react-router-dom';
-import { useState } from 'react';
-import { getCategory } from '../../products-data/categories';
+import React, { useState } from 'react';
+import { getCategory, Product } from '../../products-data/categories';
 import Quantity from './Quantity';
+import { CartItem } from '../Cart/Cart';
 
-export default function CurrentProduct() {
-  const [quantity, setQuantity] = useState(0);
-  // TODO: query from cart || 0
+interface CurrentProductProps {
+  onCartAdd: (
+    id: number,
+    quantity: number,
+  ) => (e: React.MouseEvent) => void;
+  cart: CartItem[];
+}
 
+export default function CurrentProduct({
+  onCartAdd,
+}: CurrentProductProps) {
+  const [quantity, setQuantity] = useState(1);
   const params = useParams();
-  const category = getCategory(params.categoryName!);
-  let product;
 
+  const handleQuantityChange = function (action: 'increase' | 'decrease') {
+    if (action === 'decrease') {
+      if (quantity === 1) {
+        return;
+      } else {
+        setQuantity((q) => q - 1);
+      }
+    } else if (action === 'increase') {
+      setQuantity((q) => q + 1);
+    }
+  };
+
+  const category = getCategory(params.categoryName!);
+
+  let product;
   if (category.products) {
     product = category.products.find(
-      (product) => product.id === parseInt(params.productId || ''),
+      (product) => product.id === parseInt(params.productId || '') || null,
     );
   }
 
@@ -28,8 +50,19 @@ export default function CurrentProduct() {
         <h3 className="c-current-product__name">{product.name}</h3>
         <span className="c-current-product__price">{product.price}</span>
         <div className="c-current-product__ui-wrapper">
-          <Quantity quantity={quantity} />
-          <button className="c-current-product__add-to-cart">
+          <Quantity
+            onIncrease={(e: React.MouseEvent) =>
+              handleQuantityChange('increase')
+            }
+            onDecrease={(e: React.MouseEvent) =>
+              handleQuantityChange('decrease')
+            }
+            quantity={quantity}
+          />
+          <button
+            className="c-current-product__add-to-cart"
+            onClick={onCartAdd(product.id, quantity)}
+          >
             Add to cart
           </button>
         </div>
