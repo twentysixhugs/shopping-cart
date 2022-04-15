ac0cb026b0dd8af084d7b8c1deb03c67f416864e
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-15 18:22:42 +0300

    Show 'in cart' when an item is in cart

--- src/components/Products/CurrentProduct.tsx
+++ src/components/Products/CurrentProduct.tsx
@@ -1,8 +1,9 @@
-import { useParams, Navigate } from 'react-router-dom';
+import { useParams, Navigate, Link } from 'react-router-dom';
 import React, { useState } from 'react';
 import { getCategory } from '../../products-data/categories';
 import Quantity from './Quantity';
 import { CartItem } from '../Cart/Cart';
+import { Product } from '../../products-data/categories';
 
 interface CurrentProductProps {
   onCartAdd: (
@@ -14,6 +15,7 @@ interface CurrentProductProps {
 
 export default function CurrentProduct({
   onCartAdd,
+  cart,
 }: CurrentProductProps) {
   const [quantity, setQuantity] = useState(1);
   const params = useParams();
@@ -28,7 +30,7 @@ export default function CurrentProduct({
 
   const category = getCategory(params.categoryName!);
 
-  let product;
+  let product: Product | undefined;
   if (category.products) {
     product = category.products.find(
       (product) => product.id === parseInt(params.productId || '') || null,
@@ -39,25 +41,33 @@ export default function CurrentProduct({
     return <Navigate replace to="/not-found"></Navigate>;
   }
 
+  const isInCart = !!cart.find((cartItem) => cartItem.id === product!.id);
+
   return (
     <div className="c-current-product">
       <img className="c-current-product__img" src={product.img}></img>
       <div className="c-current-product__main-wrapper">
         <h3 className="c-current-product__name">{product.name}</h3>
         <span className="c-current-product__price">{product.price}</span>
-        <div className="c-current-product__ui-wrapper">
-          <Quantity
-            onIncrease={() => handleQuantityChange('increase')}
-            onDecrease={() => handleQuantityChange('decrease')}
-            quantity={quantity}
-          />
-          <button
-            className="c-current-product__add-to-cart"
-            onClick={onCartAdd(product.id, quantity)}
-          >
-            Add to cart
-          </button>
-        </div>
+        {!isInCart ? (
+          <div className="c-current-product__ui-wrapper">
+            <Quantity
+              onIncrease={() => handleQuantityChange('increase')}
+              onDecrease={() => handleQuantityChange('decrease')}
+              quantity={quantity}
+            />
+            <button
+              className="c-current-product__add-to-cart"
+              onClick={onCartAdd(product.id, quantity)}
+            >
+              Add to cart
+            </button>
+          </div>
+        ) : (
+          <Link to="/cart" className="c-current-product__in-cart">
+            In Cart
+          </Link>
+        )}
       </div>
     </div>
   );
