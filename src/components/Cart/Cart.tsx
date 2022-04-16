1faa5eb1e8a747e3c25a0d1d8e3b64d99c14ac25
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-16 21:40:53 +0300

    Render cart conditionally, display total

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -12,21 +12,50 @@ interface CartProps {
 }
 
 export default function Cart({ cart, onItemRemove }: CartProps) {
-  return (
-    <div className="c-cart">
-      {cart.map((cartItem) => {
-        return (
-          <CartProduct
-            name={cartItem.name}
-            price={cartItem.price}
-            img={cartItem.img}
-            key={cartItem.id}
-            quantity={cartItem.quantity}
-            onRemove={() => onItemRemove(cartItem.id)}
-          />
-        );
-      })}
-      <button className="c-cart__check-out">Check out</button>
-    </div>
-  );
+  const shouldRenderCart = cart.length > 0;
+
+  if (shouldRenderCart) {
+    const total = cart.reduce((prev, cur) => {
+      return prev + parseInt(cur.price.slice(1)) * cur.quantity;
+    }, 0);
+
+    return (
+      <div className="c-cart">
+        <h1 className="c-cart__heading">Cart</h1>
+        {cart.map((cartItem) => {
+          return (
+            <CartProduct
+              name={cartItem.name}
+              price={cartItem.price}
+              img={cartItem.img}
+              key={cartItem.id}
+              quantity={cartItem.quantity}
+              onRemove={() => onItemRemove(cartItem.id)}
+            />
+          );
+        })}
+        <div className="c-cart__check-out-wrapper">
+          <span className="c-cart__total">Total: ${total}</span>
+          <button
+            className="c-cart__check-out"
+            onClick={() => {
+              alert('Thanks for coming by!');
+              cart.forEach((cartItem) => onItemRemove(cartItem.id));
+            }}
+          >
+            Check out
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="c-cart">
+        <h1 className="c-cart__heading">Cart</h1>
+        <div className="c-cart__empty-cart-wrapper">
+          There&apos;s nothing yet!
+        </div>
+      </div>
+    );
+  }
 }
