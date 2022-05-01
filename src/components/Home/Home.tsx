7f37448a320de044128cb0b4a2cc3080187f6bf1
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-01 17:20:51 +0300

    Add effect to home title

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,7 +4,9 @@ import './Home.css';
 export default function Home() {
   return (
     <div className="c-home">
-      <h1 className="c-home__title">A really clever title</h1>
+      <h1 className="c-home__title">
+        A really clever title<div className="c-home__title-effect"></div>
+      </h1>
       <p className="c-home__description">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit.
         Consequatur quia ea at delectus earum iure, nemo dignissimos amet!
